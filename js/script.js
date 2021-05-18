917544498ada30ebc33b29f6517a0f3395cb663b
Akash <akashdeshpande118@gmail.com>
2021-05-19 01:25:40 +0530

    Player controls done

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ progressbar.addEventListener('mousemove', () => {
     let color = `linear-gradient(90deg, var(--accent) ${progressbarValue}%, var(--slider-color) ${progressbarValue}%)`;
     progressbar.style.background = color;
 });
+
+//play to pause
+let playbtn = document.querySelector('.play-btn');
+
+playbtn.addEventListener('click', () => {
+    if(playbtn.classList.contains("fa-play")){
+        playbtn.classList.remove("fa-play");
+        playbtn.classList.add("fa-pause");
+    }
+    else {
+        playbtn.classList.remove("fa-pause");
+        playbtn.classList.add("fa-play");
+    }
+    
+});
